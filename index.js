ee1979ce97655f749c7eb4e26d5e7ff36bfc09f8
Frances Fisk <francesdfisk@gmail.com>
2018-04-02 21:11:11 -0700

    fixed results bar

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ $(function() {
 let TASTEDIVE_ENDPOINT = 'https://tastedive.com/api/similar',
     books,
     testing = true,
-    lightboxAllow = true;
+    lightboxAllow = false,
+    noResultsBarAllow = false;
 
 // get the data from TasteDive API
-function getDatafromTDApi(searchTerm, callback) {
+function getDataFromTDApi(searchTerm, callback) {
     // create settings for the search
     const settings = {
       data: {
@@ -33,81 +34,81 @@ function loadResults(data) {
     let results = data.Similar.Results;
     if(results.length === 0) {
         console.log("if statement fail");
-        //feedback that it failed
-        return;
+        //feedback that it failed  
+        clearResults();
+        $('.no-results-bar').removeAttr('hidden').addClass('open');
+        $('html, body').animate({                           
+            scrollTop: $(".no-results-bar").offset().top                 
+        }, 1000);
+    } else {
+        let returnHTML = "";
+        books = {};
+        // loop through each item in the Results array
+        results.forEach(function(item){
+        returnHTML += `<div class="thumbnail"><span>${item.Name}</span></div>`;
+        books[item.Name] = item;
+        });
+        clearResults();
+        $('.results-bar').removeAttr('hidden').addClass('open');
+        $('.js-search-results').html(returnHTML);
+        $('.js-search-results').addClass('show');
+        $('html, body').animate({                           
+            scrollTop: $(".results-bar").offset().top                 
+        }, 1000);
     }
-    let returnHTML = "";
-    books = {};
-    // loop through each item in the Results array
-    results.forEach(function(item){
-    returnHTML += `<div class="thumbnail"><span>${item.Name}</span></div>`;
-    books[item.Name] = item;
-    });
 
-    $('.js-search-results').html(returnHTML);
-    $('.results-bar').removeAttr('hidden').addClass('open');
-    $('.js-search-results').addClass('show');
-    $('html, body').animate({                           
-        scrollTop: $(".results-bar").offset().top                 
-    }, 2000);
     if (testing && lightboxAllow) {
         lightbox("Carrie");
     }
 }
 
-// OLD LIGHTBOX Fill lightbox with info
-// function lightbox(query) {
-//     let tasteDiveInfo = `<h3>${query}</h3>` +
-//             `<div>${books[query].wTeaser}</div>` +
-//             `<div class="wiki-link"><a href='${books[query].wUrl}' target='_blank'>Read more <i class="fas fa-external-link-alt"></i></a></div>`;
-//         $('.lightbox-content').html(tasteDiveInfo);
-//         $('.lightbox').css('display', 'block');
-// }
-
-function lightbox(query) {
-  let googleBooksInfo = `<div class="naccs">
+function lightbox(book) {
+    let displayAccordion = `
+     <div class="naccs">
         <div class="grid">
         <div class="gc gc--1-of-3">
             <div class="menu">
-            <div class="active"><span class="light"></span><span>Beer</span></div>
-            <div><span class="light"></span><span>Vine</span></div>
-            <div><span class="light"></span><span>Lemonade</span></div>
+            <div class="active"><span class="light"></span><span>Info</span></div>
+            <div><span class="light"></span><span>Buy</span></div>
+            <div><span class="light"></span><span>Description</span></div>
             </div>
         </div>
         <div class="gc gc--2-of-3">
             <ul class="nacc">
             <li class="active">
             <div>
-            <p>Beer is the world's oldest[1][2][3] and most widely consumed[4] alcoholic drink; it is the third most popular drink overall, after water and tea.[5] The production of beer is called brewing, which involves the fermentation of sugars, mainly derived
-                from cereal grain starches—most commonly from malted barley, although wheat, maize (corn), and rice are widely used.[6] Most beer is flavoured with hops, which add bitterness and act as a natural preservative, though other flavourings such as
-                herbs or fruit may occasionally be included. The fermentation process causes a natural carbonation effect, although this is often removed during processing, and replaced with forced carbonation.[7] Some of humanity's earliest known writings refer
-                to the production and distribution of beer: the Code of Hammurabi included laws regulating beer and beer parlours,[8] and "The Hymn to Ninkasi", a prayer to the Mesopotamian goddess of beer, served as both a prayer and as a method of remembering
-                the recipe for beer in a culture with few literate people.[9][10]</p>
+            <p>
+                <img src='${book.bookCover}' alt=''>
+                <span>${book.title}</span>
+                <span>${book.author}</span>
+            </p>
             </div>
             </li>
             <li>
             <div>
-            <p>A vine (Latin vīnea "grapevine", "vineyard", from vīnum "wine") in the narrowest sense is the grapevine (Vitis), but more generally it can refer to any plant with a growth habit of trailing or scandent (that is, climbing) stems, lianas or runners.
-                The word also can refer to such stems or runners themselves, for instance when used in wicker work.[1][2] In the United Kingdom, the term "vine" applies almost exclusively to the grapevine. The term "climber" is used for all climbing plants.[3]</p>
+            <p>
+                <img>
+                <span>$${book.price}</span>
+                <span><a href='${book.buyLink}' target='_blank'>Purchase</a></span>
+            </p>
             </div>
             </li>
             <li>
             <div>
-            <p>Lemonade is any of various sweetened beverages found around the world, all characterized by lemon flavor. Most lemonade varieties can be separated into two distinct types: cloudy and clear; each is known simply as "lemonade" (or a cognate) in countries
-                where dominant.[1] Cloudy lemonade, generally found in North America and India, is a traditionally homemade drink made with lemon juice, water, and sweetener such as cane sugar or honey.[2] Found in the United Kingdom, Ireland, South Africa, Australia,
-                and New Zealand, clear lemonade is a lemon flavoured carbonated soft drink. Not to be confused with Sprite a lemon-lime flavored, soft drink.</p>
+            <p>
+                <span>${book.description}</span>
+            </p>
             </div>
             </li>
             </ul>
         </div>
         </div>
-        </div>`
-        $('.lightbox-content').html(googleBooksInfo);
+        </div>`;
+        $('.lightbox-content').html(displayAccordion);
         $('.lightbox').css('display', 'block');
 }
 
-// Accordian
-// Acc
+// Accordion tabs
 $(document).on("click", ".naccs .menu div", function() {
 	var numberIndex = $(this).index();
 
@@ -130,7 +131,7 @@ $(document).on("click", ".naccs .menu div", function() {
 $('body').on('click', '.thumbnail', function() {
     let clickedElement = $(this);
     let thisText = clickedElement.text();
-    lightbox(thisText);
+    getDataFromGBApi(thisText, loadGBResults);
 })
 
 // Activate close button in lightbox
@@ -140,7 +141,8 @@ $('.inner-lightbox > button').click(function() {
 
 // Fill results bar
 function insertSearchTermOnPage(searchTerm) {
-    $('.results-bar span span').text(capitalize(searchTerm));
+    $('.results-bar-content span span').text(capitalize(searchTerm));
+    $('.no-results-bar-content span span').text(capitalize(searchTerm));
 }
 
 // Activate search button
@@ -155,7 +157,7 @@ function watchSubmit() {
         queryTarget.val("");
         insertSearchTermOnPage(query);
         // call the function that gets TasteDive data using parameters of user's input and callback function to display results
-        getDatafromTDApi(query, loadResults);
+        getDataFromTDApi(query, loadResults);
     });
 }
 
@@ -167,7 +169,8 @@ $('body').on('click', '.clear-btn', function(e) {
 )
 
 function clearResults() {
-    $('.results-bar').removeClass('open').hide();
+    $('.results-bar').removeClass('open').prop('hidden', 'hidden');
+    $('.no-results-bar').removeClass('open').prop('hidden', 'hidden');
     $('.js-search-results').removeClass('show').empty();
 }
 
@@ -191,10 +194,14 @@ $('body').on('mouseout', '.thumbnail', function() {
 
 watchSubmit();
 if (testing) { 
-    getDatafromTDApi("The Shining", loadResults);
+    getDataFromTDApi("The Shining", loadResults);
 }
 
-});
+if (noResultsBarAllow) {
+    getDataFromTDApi("dfsjlkdjfsl", loadResults);
+}
+
+
 
 // Get the Google Books Api to work
 function getDataFromGBApi(searchTerm, callback) {
@@ -215,9 +222,19 @@ function getDataFromGBApi(searchTerm, callback) {
 }
 
 function loadGBResults(data) {
-    let results = data.items[0].volumeInfo.authors;
-    console.log("google books results", results);
+    let bookInfo = {
+    title: data.items[0].volumeInfo.title,
+    author: data.items[0].volumeInfo.authors,
+    bookCover: data.items[0].volumeInfo.imageLinks.thumbnail,
+    price: data.items[0].saleInfo.retailPrice.amount,
+    buyLink: data.items[0].saleInfo.buyLink,
+    description: data.items[0].volumeInfo.description
+    };
+    console.log("google books results", bookInfo);
+    lightbox(bookInfo);
 }
 
 
-getDataFromGBApi("carrie", loadGBResults);
+// getDataFromGBApi("carrie", loadGBResults);
+
+});
